f10a9bc40a5e8134d80a5cee213df6aa91f50b3d
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-11 00:06:20 -0400

    radio buttons are now color coded

--- app/src/main/java/com/example/memoapp/MainActivity.java
+++ app/src/main/java/com/example/memoapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.memoapp;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +11,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-    private static final String Tag = "MainActivityHelper";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        setRadioButtonColor();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setRadioButtonColor() {
+        RadioButton radioButton = findViewById(R.id.radioButtonLow);
+        radioButton.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.green)));
+
+        radioButton = findViewById(R.id.radioButtonMedium);
+        radioButton.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.yellow)));
+
+        radioButton = findViewById(R.id.radioButtonHigh);
+        radioButton.setButtonTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
     }
 }
